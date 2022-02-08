a9c4e9c3a280deca72ef27d088ce7e8c99ecd797
Nikema Prophet <3941856+prophen@users.noreply.github.com>
2022-02-08 14:16:33 -0800

    Add GitHub ID and secret

--- next.config.js
+++ next.config.js
@@ -1,8 +1,9 @@
 module.exports = {
-    env: {
-      AIRTABLE_API_KEY: process.env.AIRTABLE_API_KEY,
-      AIRTABLE_BASE_ID: process.env.AIRTABLE_BASE_ID,
-      AIRTABLE_TABLE_NAME: process.env.AIRTABLE_TABLE_NAME
-    },
-  };
-  
+  env: {
+    AIRTABLE_API_KEY: process.env.AIRTABLE_API_KEY,
+    AIRTABLE_BASE_ID: process.env.AIRTABLE_BASE_ID,
+    AIRTABLE_TABLE_NAME: process.env.AIRTABLE_TABLE_NAME,
+    GITHUB_SECRET: process.env.GITHUB_SECRET,
+    GITHUB_ID: process.env.GITHUB_ID,
+  },
+};
